1800050a26fb4dd1120a802cdf2f02562dbba1d0
zmundo <zmundo36@gmail.com>
2020-03-13 20:21:42 -0400

    Add react, react dom, webpack, and babel

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Test test'));
+app.use('/', express.static('./dist', {
+    index: "index.html"
+  }))
 
 app.listen(port, () => console.log('App is listening'));
